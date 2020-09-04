7a74820dfe0686be84123025a8ffe0b57735511c
ArturFortunato <artur.fortunato1@gmail.com>
2020-09-04 12:13:29 +0100

    Ready to deploy

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import flask
 import flask_cors   
 import json
-from main import temp_main
 
 app = flask.Flask(__name__)
 flask_cors.CORS(app)
@@ -14,5 +13,3 @@ def home():
 def schedule():
     schedules = temp_main(flask.request.get_json()['courses'])
     return schedules
-
-app.run(threaded=True, port=5000)
