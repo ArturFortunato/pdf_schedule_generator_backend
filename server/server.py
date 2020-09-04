57ace0108d4e522730eebf195491c862e36e9a6f
ArturFortunato <artur.fortunato1@gmail.com>
2020-09-04 11:56:46 +0100

    Add test endpoint

--- server/server.py
+++ server/server.py
@@ -6,9 +6,12 @@ from main import temp_main
 app = flask.Flask(__name__)
 flask_cors.CORS(app)
 
+@app.route('/', methods=['GET'])
+def home():
+    return "YES"
 
 @app.route('/schedule', methods=['POST'])
-def home():
+def schedule():
     schedules = temp_main(flask.request.get_json()['courses'])
     return schedules
 
